1bc1f88beb80ec8605997260167dcff59f11b5b2
Minghao Ni <niminghao804@gmail.com>
2016-03-29 15:09:47 +0800

    add extension code

---
+++ src/inject/inject.js
@@ -0,0 +1,48 @@
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        //remove inline styles on some tables
+        $('table').each(function(index, item){
+            var item=$(item)
+            //only one code listing in table, remove table styles
+            if(item.find('pre').length===1){
+                item.removeAttr('border')
+                item.removeAttr('bgcolor')
+                item.removeClass()
+            }else{
+                item.addClass('new_table')
+            }
+        })
+
+        //add click to copy handler
+        var add_click_to_copy=function(item){
+            var ele=$(item)
+            ele.addClass('code_block')
+            var code=ele.text()
+            code=code.replace(/"/g, '&quot;')
+            var click_btn=$('<button class="click_to_copy_button">Copy</button>')
+            click_btn.attr('data-clipboard-action', 'copy')
+            click_btn.attr('data-clipboard-text', code)
+            ele.prepend(click_btn)
+        }
+        $('pre').each(function(index, item){
+            add_click_to_copy(item)
+        })
+        $('code').each(function(index, item){
+            add_click_to_copy(item)
+        })
+        window.clipboard=new Clipboard('.click_to_copy_button')
+
+        //add highlight
+        hljs.configure({
+            //languages: ['bash', 'c', 'cpp', 'python', 'perl', 'ruby', 'asm']
+        })
+        $('code').each(function(index, data){
+            hljs.highlightBlock(data)
+        })
+        $('pre').each(function(index, data){
+            hljs.highlightBlock(data)
+        })
+    }
+}, 10);
